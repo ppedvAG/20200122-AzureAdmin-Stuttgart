dc6b6c3fbeefe2b97f0f22810f9e08a680f9ce51
Stefan Ober <ppedvStefanO@users.noreply.github.com>
2020-01-22 09:05:30 +0100

    Create MakeAndExportCert.ps1
    Skript zum erstellen und exportieren von Zertifikaten von P2S Zertifikaten für Azure Gateway

---
+++ MakeAndExportCert.ps1
@@ -0,0 +1,13 @@
+$Rootcert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+-Subject "CN=P2SRootCert" -KeyExportPolicy Exportable `
+-HashAlgorithm sha256 -KeyLength 2048 `
+-CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign
+
+$clientCert = New-SelfSignedCertificate -Type Custom -DnsName P2SChildCert -KeySpec Signature `
+-Subject "CN=P2SChildCert" -KeyExportPolicy Exportable `
+-HashAlgorithm sha256 -KeyLength 2048 `
+-CertStoreLocation "Cert:\CurrentUser\My" `
+-Signer $rootcert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
+
+Export-Certificate -Cert $rootcert -FilePath $env:USERPROFILE\Desktop\rootcert.cer
+Export-Certificate -Cert $clientCert -FilePath $env:USERPROFILE\Desktop\clientcert.cer
